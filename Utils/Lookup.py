dcfe6c61c29b82ca5bdd15784f543f7f48862d72
chariskar <149198618+chariskar@users.noreply.github.com>
2023-12-13 21:31:05 +0200

    Update Lookup.py

--- Utils/Lookup.py
+++ Utils/Lookup.py
@@ -1,12 +1,13 @@
 import aiohttp
 from cachetools import TTLCache
+import json
 
 
 class Lookup:
     cache: TTLCache = TTLCache(maxsize=289, ttl=150)
 
     @classmethod
-    async def lookup(cls, server: str = 'aurora', endpoint=None, name=None,version:int=2):
+    async def lookup(cls, server: str = 'aurora', endpoint=None, name=None, version: int = 2):
         try:
 
             if endpoint is None:
@@ -15,15 +16,23 @@ class Lookup:
                 api_url = f"https://api.earthmc.net/v{version}/{server}/{endpoint}"
             else:
                 api_url = f"https://api.earthmc.net/v{version}/{server}/{endpoint}/{name}"
+            if endpoint == 'alliances':
+                api_url = f'https://emctoolkit.vercel.app/api/aurora/alliances/{name}'
             if (server, endpoint, name) in cls.cache:
                 return cls.cache[(server, endpoint, name)]
             async with aiohttp.ClientSession() as session:
                 async with session.get(api_url) as response:
-                    lookup = await response.json()
-           
-            cls.cache[(server, endpoint, name)] = lookup
-            return lookup
+                    response_text = await response.text()
 
+                    content_type = response.headers.get('Content-Type', '').lower()
+                    if 'application/json' in content_type:
+                        lookup = json.loads(response_text)
+                    else:
+                        lookup = response_text
+
+                    cls.cache[(server, endpoint, name)] = lookup
+
+            return cls.cache.get((server, endpoint, name))
         except Exception as e:
             raise e
 
